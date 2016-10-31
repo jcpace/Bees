2bd32937fbd60c0518ddf61c80a1d0631ef653e0
Joshua Pace <joshpace@gmail.com>
2016-10-31 11:25:02 -0700

    Initial Commit

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -8,7 +8,7 @@ this.treasureChest = [];
 // food property inherited from Grub
 // eat method inherited from Grub
 };
-RetiredForagerBee.prototype.forrage = function(){
+RetiredForagerBee.prototype.forage = function(){
 	return "I am too old, let me play cards instead";
 };
 RetiredForagerBee.prototype.gamble = function(treasure){
